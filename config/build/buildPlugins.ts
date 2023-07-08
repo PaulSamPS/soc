d6d809f06fa3bf1c43febfab6fa492eaf6edb3bc
PaulSamPS <psamoylenko2@gmail.com>
2023-07-08 18:37:44 +0300

    babal, i18next extract plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -14,10 +14,9 @@ export const buildPlugins = ({paths, isDev}: BuildOptions): webpack.WebpackPlugi
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }),
-
-
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ]
 }
